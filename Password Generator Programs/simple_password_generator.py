aeb076caf518dbaa9f2d886e05d0f260f087249f
sreekeshiyer <sreekesh.subramanian@gmail.com>
2021-09-17 12:57:19 +0530

    added comments

--- Password Generator Programs/simple_password_generator.py
+++ Password Generator Programs/simple_password_generator.py
@@ -4,4 +4,8 @@ from characters import letters, numbers, symbols
 print("Welcome to the Python Password Generator!")
 no_of_characters = int(input("How many characters would you like?\n"))
 
+
+### Taking a random sample from the list of characters, the sample size is same as the number of characters
+### as mentioned in the parameters. Using the join method to make a string out of this list. 
+
 print('Password - ',''.join(random.sample(letters+numbers+symbols, no_of_characters)))
