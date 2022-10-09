35e84a5bc485a213a7d1abf5d43a1e97c5b9afcf
SirRomey <71877936+SirRomey@users.noreply.github.com>
2022-10-09 15:43:48 +0530

    Update language_translate.py

--- Language-Translate/language_translate.py
+++ Language-Translate/language_translate.py
@@ -3,6 +3,7 @@ from translate import Translator
 
 
 def main():
+    # Initializing Argument Parser
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--from_lang", help="Language to translate from.", type=str)
@@ -11,11 +12,14 @@ def main():
         type=str)
     text = input("Enter text to translate: ")
     args = parser.parse_args()
+    # Checking if a from_language is provided. (defaults automatically to English)
+    # If provided, set from_language in the Translator object.
     if args.from_lang:
         translator = Translator(
             to_lang=args.to_lang if args.to_lang else "English",
             from_lang=args.from_lang
         )
+    # Else, do not pass anything for from_language.
     else:
         translator = Translator(
             to_lang=args.to_lang if args.to_lang else "English")
